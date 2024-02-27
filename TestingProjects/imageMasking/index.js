92064a2f87432aced857bde09e83a9359beb5bf8
Matthew Mason <m.mason29272@gmail.com>
2024-02-27 16:25:59 +0000

    Updated colour scheme and transitions

--- TestingProjects/imageMasking/index.js
+++ TestingProjects/imageMasking/index.js
@@ -6,14 +6,15 @@ const skipButton = document.getElementById('skipButton');
 
 // -------Event Listeners-------
 loader.addEventListener('animationend', () => {
-  loader.style.opacity = '0%';
   loader.style.transition = 'opacity, 2s';
-  body.style.backgroundColor = '#001F00';
+  loader.style.opacity = '0%';
+  loader.style.zIndex = '0';
   body.style.transition = 'backgroundColor, 2s';
-  skipText.style.opacity = '0%';
+  body.style.backgroundColor = '#083829';
   skipText.style.transition = 'opacity 1s';
-  skipButton.style.opacity = '0%';
+  skipText.style.opacity = '0%';
   skipButton.style.transition = 'opacity 1s';
+  skipButton.style.opacity = '0%';
 })
 
 skipButton.addEventListener('mouseenter', () => {
@@ -27,11 +28,17 @@ skipButton.addEventListener('mouseleave', () => {
 })
 
 skipButton.addEventListener('mousedown', () => {
-  skipText.style.transition = 'fontSize, 0s';
+  skipText.style.transition = 'fontSize 0s, opacity 1s;';
+  skipText.style.opacity = '0%';
   skipText.style.fontSize = '28';
-  loader.style.transition = 'opacity 1s';
+  loader.style.transition = 'opacity 2.6s 1s, zIndex 2.6s 1s';
   loader.style.opacity = '0%';
-  body.style.backgroundColor = '#001F00';
+  loader.style.zIndex = '5';
+  skipButton.style.transition = 'r .9s ease-in-out';
+  skipButton.style.r = '1250';
+  skipButton.style.fill = 'url(#grad1)';
+  skipButton.style.filter = 'drop-shadow(0 0 200px #083829)';
+  body.style.backgroundColor = '#083829';
 })
 
 skipButton.addEventListener('mouseup', () => {
