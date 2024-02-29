ff02d48f7bb8aebc49cb2098d4138e37f8fd050d
Matthew Mason <m.mason29272@gmail.com>
2024-02-29 02:02:49 +0000

    continued updating website, nav bar, main content and testing designs

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -3,6 +3,7 @@ const loader = document.querySelector('svg');
 const body = document.querySelector('body');
 const skipText = document.getElementById('skipText');
 const skipButton = document.getElementById('skipButton');
+const mainContent = document.getElementById('main');
 
 // -------Event Listeners-------
 loader.addEventListener('animationend', () => {
@@ -13,11 +14,12 @@ loader.addEventListener('animationend', () => {
   skipText.style.opacity = '0%';
   skipButton.style.transition = 'opacity 1s';
   skipButton.style.opacity = '0%';
+  mainContent.style.display = 'block';
   setTimeout(function () {
     loader.addEventListener('transitionend', function () {
       loader.style.display = 'none';
     })
-  }, 2000)
+  }, 1500)
 })
 
 skipButton.addEventListener('mouseenter', () => {
@@ -26,21 +28,22 @@ skipButton.addEventListener('mouseenter', () => {
 })
 
 skipButton.addEventListener('mouseleave', () => {
-  skipText.style.transition = 'fontSize, 0s';
-  skipText.style.fontSize = '28';
+  skipText.style.transition = 'fontSize, 1s';
+  skipText.style.fontSize = '40';
 })
 
 skipButton.addEventListener('mousedown', () => {
-  skipText.style.transition = 'fontSize 0s, opacity 1s;';
+  skipText.style.transition = 'fontSize 3s, opacity 1s;';
   skipText.style.opacity = '0%';
   skipText.style.fontSize = '28';
-  loader.style.transition = 'opacity 2.6s 1s, zIndex 2.6s 1s';
   loader.style.opacity = '0%';
   loader.style.zIndex = '5';
-  skipButton.style.transition = 'r .9s ease-in-out';
+  skipButton.style.transition = 'r 1s ease-in-out';
   skipButton.style.r = '1250';
   skipButton.style.fill = 'url(#grad1)';
   skipButton.style.filter = 'drop-shadow(0 0 200px #083829)';
+  loader.style.transition = 'opacity 1s 1s, zIndex 2s 1s';
+  mainContent.style.display = 'block';
   setTimeout(function () {
     loader.addEventListener('transitionend', function () {
       loader.style.display = 'none';
