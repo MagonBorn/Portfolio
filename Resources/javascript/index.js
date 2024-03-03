a4b04140e3bcd0deceebfaa2a8bdd7f9bdcfd810
Matthew Mason <m.mason29272@gmail.com>
2024-03-03 17:53:16 +0000

    cleaned up code, fixed animation and screen fade from loading screen to home page

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -1,57 +1,66 @@
-// -------DOM Variables-------
-const loader = document.querySelector('svg');
+// // -------DOM Variables-------
+const loadingContainer = document.querySelector('svg');
 const body = document.querySelector('body');
 const skipText = document.getElementById('skipText');
 const skipButton = document.getElementById('skipButton');
-const mainContent = document.getElementById('main');
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const mainContent = document.querySelector('main');
+const aboutMe = document.getElementById('aboutMe');
 
-// -------Event Listeners-------
-loader.addEventListener('animationend', () => {
-  loader.style.transition = 'opacity, 2s';
-  loader.style.opacity = '0%';
-  loader.style.zIndex = '0';
-  skipText.style.transition = 'opacity 1s';
-  skipText.style.opacity = '0%';
-  skipButton.style.transition = 'opacity 1s';
-  skipButton.style.opacity = '0%';
-  mainContent.style.display = 'block';
-  setTimeout(function () {
-    loader.addEventListener('transitionend', function () {
-      loader.style.display = 'none';
-    })
-  }, 1500)
-})
-
-skipButton.addEventListener('mouseenter', () => {
-  skipText.style.transition = 'fontSize, 1s';
-  skipText.style.fontSize = '32';
-})
-
-skipButton.addEventListener('mouseleave', () => {
-  skipText.style.transition = 'fontSize, 1s';
-  skipText.style.fontSize = '40';
-})
-
+// -------On Skip Button clicked-------
 skipButton.addEventListener('mousedown', () => {
-  skipText.style.transition = 'fontSize 3s, opacity 1s;';
-  skipText.style.opacity = '0%';
-  skipText.style.fontSize = '28';
-  loader.style.opacity = '0%';
-  loader.style.zIndex = '5';
   skipButton.style.transition = 'r 1s ease-in-out';
   skipButton.style.r = '1250';
   skipButton.style.fill = 'url(#grad1)';
   skipButton.style.filter = 'drop-shadow(0 0 200px #083829)';
-  loader.style.transition = 'opacity 1s 1s, zIndex 2s 1s';
-  mainContent.style.display = 'block';
-  setTimeout(function () {
-    loader.addEventListener('transitionend', function () {
-      loader.style.display = 'none';
-    })
-  }, 2000)
-})
 
-skipButton.addEventListener('mouseup', () => {
-  skipText.style.transition = 'fontSize, 1s';
-  skipText.style.fontSize = '32';
-})
+  skipText.style.transition = 'fontSize, 3s';
+  skipText.style.fontSize = '400';
+  skipText.innerHTML = 'Welcome';
+
+  body.style.transition = 'backgroundColor, 6s';
+  body.style.backgroundColor = 'white';
+
+  loadingContainer.classList.add('visibilityHidden');
+
+  loadingContainer.addEventListener('transitionend', (e) => {
+    if (e.propertyName === 'opacity') {
+      loadingContainer.classList.add('hidden');
+      header.classList.remove('hidden');
+      mainContent.classList.remove('hidden');
+      footer.classList.remove('hidden');
+      setTimeout(() => {
+        header.classList.remove('visibilityHidden');
+        mainContent.classList.remove('visibilityHidden');
+        footer.classList.remove('visibilityHidden');
+      }, 20);
+    }
+  })
+});
+
+// -------On Animation End-------
+loadingContainer.addEventListener('animationend', () => {
+  skipText.style.transition = 'opacity 1s';
+  skipText.style.opacity = '0%';
+  skipButton.style.transition = 'opacity 1s';
+  skipButton.style.opacity = '0%';
+
+  loadingContainer.classList.add('visibilityHidden');
+
+  loadingContainer.addEventListener('transitionend', (e) => {
+    if (e.propertyName === 'opacity') {
+      body.style.transition = 'backgroundColor, 8s';
+      body.style.backgroundColor = 'white';
+      loadingContainer.classList.add('hidden');
+      header.classList.remove('hidden');
+      mainContent.classList.remove('hidden');
+      footer.classList.remove('hidden');
+      setTimeout(() => {
+        header.classList.remove('visibilityHidden');
+        mainContent.classList.remove('visibilityHidden');
+        footer.classList.remove('visibilityHidden');
+      }, 20);
+    }
+  })
+});
