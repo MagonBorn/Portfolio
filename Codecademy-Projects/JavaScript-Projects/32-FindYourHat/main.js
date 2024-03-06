46cb022ab87045d82ffa6900adae7fbce4af4386
Matthew Mason <m.mason29272@gmail.com>
2024-03-06 12:51:11 +0000

    Added FindYourHat terminal game from Codecademy Project

---
+++ Codecademy-Projects/JavaScript-Projects/32-FindYourHat/main.js
@@ -0,0 +1,216 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+// ----------Variables----------
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+let moveDirection = '';
+
+// ----------Field Class----------
+class Field {
+  // Constructor
+  constructor(field) {
+    this._field = field;
+    this.playerLocation = []
+    this.startingPosition;
+    console.log(`Welcome to \'Find Your Hat\'.\n\nTo play, simply type the direction you wish to move:\n\'u\' for up\n\'d\' for down\n\'l\' for left\n\'r\' for right.\n\nThe rules are simple:\n1. Don't fall down any holes\n2. Don't fall off the edge\n3. You can't return to a space you've already visited\n4. Find Your Hat\n`);
+  }
+
+  // ----------Getters----------
+  // Get the starting position of the player
+  get startingPosition() {
+    for (let i = 0; i < this._field.length; i++) {
+      for (let j = 0; j < this._field[i].length; j++) {
+        if (this._field[i][j] === '*') {
+          this.playerLocation.push(i, j);
+        }
+      }
+    }
+    return this.playerLocation;
+  }
+
+  // ----------Methods----------
+  // Game loop
+  runGame() {
+    console.log(this.print());
+    this.movePlayer(this.checkUserInput());
+  }
+
+  // Display play area as a grid in the terminal
+  print() {
+    let line = [];
+    for (let fieldRow in this._field) {
+      line += this._field[fieldRow].join('') + '\n';
+    }
+    return line;
+  }
+
+  // Check the user has provided a vlid movement input
+  checkUserInput() {
+    let direction = prompt('Which direction do you wish to move? ').toLowerCase();
+    if (!/[udlrnews]/.test(direction) === true) {
+      console.log(`ALERT: Invalid direction: please type \n\'u\' for up, \n\'d\' for down, \n\'r\' for right, \n\'l\' for left.\n`)
+      this.checkUserInput();
+    } else {
+      return direction;
+    }
+  }
+
+  // Move the player in the direction indicated by the user input
+  movePlayer(direction) {
+    let newLocation = []
+    switch (direction) {
+      case 'u': case 'n':
+        newLocation.push(this.playerLocation[0] - 1, this.playerLocation[1]);
+        if (newLocation[0] < 0) {
+          this.endGame();
+        } else {
+          this.locationChecks(newLocation);
+        }
+        break;
+      case 'd': case 's':
+        newLocation.push(this.playerLocation[0] + 1, this.playerLocation[1]);
+        if (newLocation[0] === this._field.length) {
+          this.endGame();
+        } else {
+          this.locationChecks(newLocation);
+        }
+        break;
+      case 'r': case 'e':
+        newLocation.push(this.playerLocation[0], this.playerLocation[1] + 1);
+        if (newLocation[1] === this._field[0].length) {
+          this.endGame()
+        } else {
+          this.locationChecks(newLocation);
+        }
+        break;
+      case 'l': case 'w':
+        newLocation.push(this.playerLocation[0], this.playerLocation[1] - 1);
+        if (newLocation[1] < 0) {
+          this.endGame();
+        } else {
+          this.locationChecks(newLocation);
+        }
+    }
+  }
+
+  // Run a number of checks against the users position
+  locationChecks(arr) {
+    this.checkForHole(arr);
+    if (this.checkForStar(arr) === true) {
+      this.updateMap(this.playerLocation);
+    };
+    this.checkForHat(arr);
+    this.updateMap(arr);
+  }
+  // Check the players intended move for holes
+  checkForHole(arr) {
+    if (this._field[arr[0]][arr[1]] === 'O') {
+      this.endGame();
+    }
+  }
+
+  // Check the players intended move for backtracking
+  checkForStar(arr) {
+    if (this._field[arr[0]][arr[1]] === '*') {
+      console.log(`\nYou can't return to this space\n`);
+      return true;
+    }
+  }
+
+  // Check the plays intended move for their hat
+  checkForHat(arr) {
+    if (this._field[arr[0]][arr[1]] === '^') {
+      this.winGame()
+    }
+  }
+
+  // Update map to show player position and route
+  updateMap(arr) {
+    this._field[arr[0]][arr[1]] = '*';
+    this.playerLocation[0] = arr[0];
+    this.playerLocation[1] = arr[1]
+    this.runGame();
+  }
+
+  // Lose condition
+  endGame() {
+    console.log(`You Have lost`);
+    let replay = prompt(`Do you want to play again? \'y\' for yes, \'n\' for no `).toLowerCase();
+    let newRows = prompt(`\nSpecify the number of rows, e.g., 4: `);
+    let newCols = prompt(`\nSpecify the number of columns, e.g., 4: `);
+    let newHoles = prompt(`\nSpecify the difficulty: what percent of the grid should be covered in holes?  `);
+    if (replay === 'y') {
+      let newGrid = new Field(Field.generateField(newRows, newCols, newHoles))
+      newGrid.runGame();
+    } else {
+      console.log(`Thank for you playing. Goodbye`);
+      throw new Error(`End Game`);
+    }
+  }
+
+  // Win condition
+  winGame() {
+    console.log(`You have Won, thanks for playing`);
+    throw new Error(`WINNER`);
+  }
+
+  // Generate a random grid pattern based on passed in rows, columns and percentage of holes that should be in the grid
+  static generateField(row, col, percent) {
+    let field = [];
+    const tiles = [];
+    let randRow = Math.floor(Math.random() * row);
+    let randCol = Math.floor(Math.random() * col);
+    let hatPos = true;
+    // Populate the tiles array with supplied percentage of holes and path tiles
+    for (let i = Math.floor(row * col * percent / 100) + 1; i > 0; i--) {
+      tiles.push('O');
+    }
+    for (let i = Math.floor(row * col * (100 - percent) / 100) + 1; i > 0; i--) {
+      tiles.push('░');
+    }
+    // Construct the grid and remove used tile from tiles array
+    for (let i = row; i > 0; i--) {
+      let gridRow = [];
+      for (let j = col; j > 0; j--) {
+        let tileIndex = Math.floor(Math.random() * tiles.length);
+        if (tiles[tileIndex] === '░') {
+          gridRow.push(tiles[tileIndex]);
+          tiles.splice(tileIndex, 1);
+        }
+        if (tiles[tileIndex] === 'O') {
+          gridRow.push(tiles[tileIndex]);
+          tiles.splice(tileIndex, 1);
+        }
+      }
+      // Add completed row to the grid
+      field.push(gridRow);
+    }
+    // Place hat in random position on the grid
+    field[Math.floor(Math.random() * row)][Math.floor(Math.random() * col)] = '^';
+    // Check to make sure the player isn't placed on the same position as the hat
+    while (hatPos) {
+      if (field[randRow][randCol] !== '^') {
+        field[randRow][randCol] = '*';
+        hatPos = false;
+      } else {
+        randRow = Math.floor(Math.random() * row);
+        randCol = Math.floor(Math.random()* col);
+      }
+    }
+    return field;
+  }
+}
+
+
+const myField = new Field(Field.generateField(5, 20, 25));
+// Create new Field Class
+// const myField = new Field([
+//   ['░', '░', 'O'],
+//   ['░', '*', '░'],
+//   ['░', '^', '░'],
+// ]);
+
+// Start Game
+myField.runGame();
