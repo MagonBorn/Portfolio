b276d653237541237dc807c4d14bd41ae5b0a6a0
Matthew Mason <m.mason29272@gmail.com>
2024-05-06 14:58:51 +0100

    Added slideshow of images to profile page

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -74,3 +74,22 @@ window.addEventListener('resize', function () {
   body.style.transition = 'none';
   header.style.transition = 'none';
 })
+
+// Slideshow Code
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("imageSlides");
+  var y = document.getElementsByClassName('textSlides');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    y[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.display = "block";
+  y[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 10000);
+}
