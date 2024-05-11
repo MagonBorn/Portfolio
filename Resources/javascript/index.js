9911919fad750483634b0a943d385068a2a1327b
Matthew Mason <m.mason29272@gmail.com>
2024-05-11 10:54:54 +0100

    updated Javascript files for each page to remove duplicate code

--- Resources/javascript/index.js
+++ Resources/javascript/index.js
@@ -94,13 +94,3 @@ function carousel() {
   setTimeout(carousel, 10000);
 }
 
-// -------Open Mobile Nav Menu-------
-function openMenu(x) {
-  let mobileMenu = document.getElementById('mobileMenu');
-  x.classList.toggle('change');
-  if (mobileMenu.style.display === 'block') {
-    mobileMenu.style.display = 'none';
-  } else {
-    mobileMenu.style.display = 'block';
-  }
-}
