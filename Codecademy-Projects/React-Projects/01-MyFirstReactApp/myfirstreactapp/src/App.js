7de37d5495136df6c51695661d13d7fbca4329a1
Matthew Mason <m.mason29272@gmail.com>
2024-07-02 13:00:37 +0100

    changed myfirstreactapp as part of codecademy testing/learning

--- Codecademy-Projects/React-Projects/01-MyFirstReactApp/myfirstreactapp/src/App.js
+++ Codecademy-Projects/React-Projects/01-MyFirstReactApp/myfirstreactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Codecademy!
+          My First React App.
         </p>
         <a
           className="App-link"
